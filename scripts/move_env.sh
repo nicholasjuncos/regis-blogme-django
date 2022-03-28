61487e095e744fd7e233274b1db272499b2748a5
Nicholas Juncos <nicholas.juncos@protonmail.com>
2022-03-28 16:11:47 -0500

    Added useless css file to ensure no command breaks. Also removed useless commented out commands.

--- scripts/move_env.sh
+++ scripts/move_env.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# May not need environment variables below!
 sudo cp /home/ubuntu/environment /home/ubuntu/website/.env
 sudo chown -R ubuntu /home/ubuntu/website/.env
 
@@ -8,7 +9,6 @@ sudo mkdir /home/ubuntu/.aws
 sudo rm /etc/nginx/sites-enabled/default
 sudo mv /home/ubuntu/website/scripts/files/nginx.conf /etc/nginx/nginx.conf
 sudo mv /home/ubuntu/website/scripts/files/website.conf /etc/supervisor/conf.d/website.conf
-#sudo mv /home/ubuntu/website/scripts/files/frontend.conf /etc/supervisor/conf.d/frontend.conf
 sudo mv /home/ubuntu/website/scripts/files/hosts /etc/hosts
 # sudo mv /home/ubuntu/website/500.html /usr/share/nginx/html/500.html
 sudo mv /home/ubuntu/website/scripts/files/conf /home/ubuntu/.aws/conf
