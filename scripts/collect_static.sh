531c4589aac892aef497ab82e4b71cc68f923747
Nicholas Juncos <nicholas.juncos@protonmail.com>
2022-03-31 17:55:16 -0500

    Fixed error for collect_static command

--- scripts/collect_static.sh
+++ scripts/collect_static.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+source /home/ubuntu/env/bin/activate
+cd /home/ubuntu/website
 sudo mkdir /home/ubuntu/website/staticfiles
 sudo chmod -R 777 /home/ubuntu/website/staticfiles
 ./manage.py collectstatic
