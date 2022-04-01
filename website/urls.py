a3d77a53aad039de5ccfa8a667838640c6529b79
Nicholas Juncos <nicholas.juncos@protonmail.com>
2022-03-31 19:28:34 -0500

    Added renderer_class needed for health_check to work.

--- website/urls.py
+++ website/urls.py
@@ -22,6 +22,8 @@ from django.views import defaults as default_views
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
 import requests
 
 
@@ -62,6 +64,8 @@ def make_request(request_type, route, data=None):
         return None
 
 
+@api_view(('GET',))
+@renderer_classes((JSONRenderer,))
 def health_check(request):
     return Response(status=status.HTTP_200_OK)
 
